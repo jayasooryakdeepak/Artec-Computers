4b4c6f06690c7c26f3c637a6658d6b0ce3062fd8
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2023-09-03 12:12:15 +0530

    Registration data added to DB Success

--- insert_values.php
+++ insert_values.php
@@ -1,4 +1,7 @@
 <html>
+<head>
+  <title>Inserted</title>
+</head>
 <body>
 
 <?php
@@ -14,16 +17,15 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$id = $_POST['book_id'];
-$name = $_POST['book_name'];
-$email = $_POST['author'];
-$phone = $_POST['quantity'];
-$place = $_POST['price']; 
-$user_name = $_POST[''];
-$passwo = $_POST['email'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$phone = $_REQUEST['phone'];
+$user_name = $_REQUEST['user_name'];
+$passwo = $_REQUEST['passwo'];
+$place = $_REQUEST['place'];
 
-$sql = "INSERT INTO book (bookid, bookname, author, email, price, qty)
-VALUES ('$book_id','$book_name', '$author', '$email','$price','$quantity')";
+$sql = "INSERT INTO Customer_details (Cust_Name, Email, Phone, user_name, Passwo, Place)
+VALUES ('$name', '$email','$phone','$user_name', '$passwo','$place')";
 
 if ($conn->query($sql) === TRUE) {
   echo "Record Added";
